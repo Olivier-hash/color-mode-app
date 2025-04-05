03d369b52c88ed9d0da040381491fe7db541fe6c
olivier Rukundo <rr.olivier01@gmail.com>
2025-04-05 11:18:10 +0200

    fixed toggling

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,2 +1,17 @@
 import { useTheme } from "../context/ThemeContext";
 import './themeToggle.css';
+
+const Themetoggle = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className="theme-toggle-container">
+      <button className={`theme-toggle ${theme}`}  onClick={toggleTheme}>
+        Toggle to {theme === "light" ? "Dark" : "Light"} Theme
+      </button>
+    </div>
+  );
+};
+
+export default Themetoggle;
+
